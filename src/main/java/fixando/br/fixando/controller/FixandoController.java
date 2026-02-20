863904f314ba3db50bd4b8ca8d6d2d57566d34a9
Luis Valdo <luisvso.dev@gmail.com>
2026-02-20 00:15:00 -0300

    feat:The controller now serialize from String to JSON, cleaning the String from the beggining to the end of the String

--- src/main/java/fixando/br/fixando/controller/FixandoController.java
+++ src/main/java/fixando/br/fixando/controller/FixandoController.java
@@ -7,6 +7,7 @@ import fixando.br.fixando.model.service.FlashCardService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import tools.jackson.databind.ObjectMapper;
 
 @Slf4j
 @RestController
@@ -22,14 +23,24 @@ public class FixandoController {
     }
 
     @PostMapping("/generate")
-    public ResponseEntity<FlashCardResponse> create(@RequestBody FlashCardRequest flashCardRequest) {
+    public ResponseEntity<FlashCardResponse> create(@RequestBody FlashCardRequest flashCardRequest) throws Exception {
 
         log.info("Trying to acces the env variable: " +  System.getenv("GOOGLE_API_KEY") );
 
         String responsePrompt = flashCardService.generateFlashCards(flashCardRequest);
 
-        return ResponseEntity.ok(new FlashCardResponse(flashCardMapper.resultToFlashcard()));
+        // creating an object mapper to map the JSON to the Record
+        ObjectMapper objectMapper = new ObjectMapper();
 
+        // replacing the begining of the prompt and the end, so the JSON can interpretate properly
+        String json = responsePrompt.replaceAll("```json", "").replaceAll("```", "").trim();
+
+        // mapping the JSON into a flashCardResponse
+        FlashCardResponse result = objectMapper.readValue(json, FlashCardResponse.class);
+
+        log.info("Tentando retornar : " + result.toString());
+
+        return ResponseEntity.ok(result);
     }
 
 }
