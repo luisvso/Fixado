115ff4f4e8b3f321a810e2ec6bb0fb5165f2c61a
Luis Valdo <luisvso.dev@gmail.com>
2026-02-20 23:37:18 -0300

    feat:A message for validating the number of flashcards was created

--- src/main/java/fixando/br/fixando/dto/FlashCardRequest.java
+++ src/main/java/fixando/br/fixando/dto/FlashCardRequest.java
@@ -1,8 +1,9 @@
 package fixando.br.fixando.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record FlashCardRequest(
         @NotBlank(message = "The input text should not be blank, please insert some text.") String text,
-        int flashNumber) {
+        @NotNull(message = "Please select the number of flashCards you want to generate") int flashNumber) {
 }
